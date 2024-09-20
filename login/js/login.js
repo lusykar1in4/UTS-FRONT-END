22a22112a990c55bd7a0663611d87d8d1cdc06f5
ChelaLorenchia <chela.lorenchia277@gmail.com>
2024-09-20 20:39:45 +0700

    login done

--- login/js/login.js
+++ login/js/login.js
@@ -9,3 +9,25 @@ registerLink.addEventListener('click', () => {
 loginLink.addEventListener('click', () => {
     logregBox.classList.remove('active');
 });
+
+document.getElementById("registerForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const nama = document.getElementById("nama").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    localStorage.setItem(nama, email);
+    localStorage.setItem(email, password);
+    alert("Pendaftaran berhasil");
+
+    window.location.href = "home.html";
+});
+
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    sessionStorage.setItem("loggedIn", "true");
+
+    window.location.href = "home.html";
+});
